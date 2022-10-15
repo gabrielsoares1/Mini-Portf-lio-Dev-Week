9a46fa93590004e1333672fb4de4596fc6469f36
Gabriel Teixeira Soares <104389736+gabrielsoares1@users.noreply.github.com>
2022-10-14 22:24:04 -0300

    Add files via upload

---
+++ src/javascript/index.js
@@ -0,0 +1,16 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function () {
+        const abaSelecionada = document.querySelector(".aba.selecionado");
+        abaSelecionada.classList.remove("selecionado")
+        aba.classList.add("selecionado")
+
+        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+        informacaoSelecionada.classList.remove("selecionado");
+        const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+        const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+        informacaoASerMostrada.classList.add("selecionado")
+    });
+});
